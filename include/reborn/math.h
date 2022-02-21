a276a9ae25381e6c7d3065f20b8d635c8b1100aa
Corendos <corentin.godeau37@gmail.com>
2022-02-22 00:24:37 +0100

    feat: added gcd functions in math.h

--- include/reborn/math.h
+++ include/reborn/math.h
@@ -7,7 +7,29 @@
 #ifndef REBORN_MATH_H
 #define REBORN_MATH_H
 
+#include <reborn/types.h>
+
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) < (b) ? (b) : (a))
+
+inl u64 gcd(u64 a, u64 b) {
+    while (b != 0) {
+        u64 temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+inl i64 gcd(i64 a, i64 b) {
+    while (b != 0) {
+        i64 temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
 
 #endif // REBORN_MATH_H
